31738685a316adaada3a77698a10fb160d64c576
Abdalla <qoryoley@gmail.com>
2023-03-23 22:37:57 +0300

    Completed Arithmetic

---
+++ index.js
@@ -0,0 +1,34 @@
+const add=(a, b) =>{
+    return a+b;
+}
+
+
+const subtract=(a, b)=> {
+    return a-b
+}
+const multiply=(a, b)=>{
+    return a*b;
+}
+const divide=(a, b)=> {
+    return a/b;
+}
+
+var n = 0;
+
+function increment(n){
+
+  n++;
+  return n;
+}
+var n=0;
+function decrement (n){
+    n--;
+    return n;
+}
+function makeInt(string){
+    return parseInt(string,10)
+  }
+  function preserveDecimal(string){
+    return parseFloat(string)
+  }
+  
